a12ed75903eca6085ef34a2ed40cb4d472aa00e9
sidSing <siddhartsingh@hotmail.com>
2022-02-10 15:07:09 +0530

    Added game logic

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 'use strict';
 
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+// document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
+  // No input
   if (!guess) {
-    document.querySelector('.message').textContent = `🚫 No Number!`;
+    document.querySelector('.message').textContent = `⛔ No number!`;
+
+    // Player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🥳 Corrent number!`;
+
+    document.querySelector('body').style.backgroundColor;
+
+    // Guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.score').textContent = --score;
+      document.querySelector('.message').textContent = '📈 Too high!';
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = `💥 You lost the game!`;
+    }
+
+    //Guess is too low
+  } else {
+    if (score > 1) {
+      document.querySelector('.score').textContent = --score;
+      document.querySelector('.message').textContent = '📉 Too low!';
+    } else {
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = `💥 You lost the game!`;
+    }
   }
 });
